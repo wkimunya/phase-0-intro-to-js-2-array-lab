762faa73982b396a95207b6be0a4f976db5fec7c
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-07-03 01:18:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+var Cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph)
+    return cats
+}
+ 
+var Cats = ["Milo", "Otis", "Garfield"]
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob)
+    return cats
+}
+
+var Cats = ["Milo", "Otis", "Garfield"]
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats
+}
+
+var Cats = ["Milo", "Otis", "Garfield"]
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats 
+}
+
+var Cats = ["Milo", "Otis", "Garfield"]
+const copyOfCats = [...cats];
+function appendCat(Broom) {
+    const allcats = [...cats, "Broom"]
+    return allcats
+}
+
+var Cats = ["Milo", "Otis", "Garfield"]
+function prependCat(Arnold) {
+    const allcats= [Arnold, ...cats]
+    return allcats
+}
+
+var Cats = ["Milo", "Otis", "Garfield"]
+function removeLastCat () {
+   var caats = cats.slice(0,cats.length-1);
+    return caats;
+}
+
+var Cats = ["Milo", "Otis", "Garfield"]
+function removeFirstCat() {
+    var catts = cats.slice(1)
+    return catts
+}
